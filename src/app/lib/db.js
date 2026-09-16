8dca3576477b9dadf5ec8f70eb704433184ca228
ypiinsanqurani <ypiinsanquranii@gmail.com>
2026-09-16 09:29:35 +0700

    Update db.js

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,1 +1,8 @@
+import mysql from 'mysql2/promise';
 
+export const db = mysql.createPool({
+  host: process.env.DB_HOST || 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+  user: process.env.DB_USER || '3KJRebnr8LRAvc7.root',
+  password: process.env.DB_PASSWORD || '20tJKULPxlCwC2YQ',
+  database: process.env.DB_DATABASE || 'db_yayasan',
+});
