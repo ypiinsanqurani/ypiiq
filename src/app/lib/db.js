f1924ae01b9a74db872c9cea0b803cfb9209ea18
ypiinsanqurani <ypiinsanquranii@gmail.com>
2026-09-21 08:53:13 +0700

    Update db.js

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
-  port: Number(process.env.DB_PORT) || 4000,
-  user: process.env.DB_USER || '3KJRebnr8LRAvc7.root',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE || 'db_yayasan',
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT) || 4000,
   ssl: {
     minVersion: 'TLSv1.2',
     rejectUnauthorized: true,
